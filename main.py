6c88cbe81f2cdfaae18c7e43187503109bfb69df
Mohammed Nawaz <moha4465@mcw0c8p3yy.home>
2020-09-13 16:48:08 +0100

    Add abort function and delete functionality, and update tests.

--- main.py
+++ main.py
@@ -15,15 +15,25 @@ def abort_nonexistent_video_id(video_id):
     if video_id not in videos:
         abort(404, message="Video id is not valid...")
 
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="A video with this ID already exists...")
+
 class Video(Resource):
     def get(self, video_id):
         abort_nonexistent_video_id(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_nonexistent_video_id(video_id)
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,8 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"name": "Grape!", "views": 1000000, "likes": 10})
-print(response.json())
+data = [
+    {"name": "Grape!", "views": 1000000, "likes": 9383942},
+    {"name": "Falling", "views": 3456, "likes": 145},
+    {"name": "Crazy Jump", "views": 1343, "likes": 12323}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+
 input()
-response = requests.get(BASE + "video/1")
+reponse = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
