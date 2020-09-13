11c591f1b55058000078d98853822633567a7cc3
Mohammed Nawaz <moha4465@mcw0c8p3yy.home>
2020-09-13 14:52:22 +0100

    Passing in parameters and returing information.

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+names = {
+    "jeff": {"age": 45, "gender": "male"},
+    "alex": {"age": 26, "gender": "male"},
+    "lucy": {"age": 27, "gender": "female"}
+}
 
-api.add_resource(HelloWorld, "/helloworld")
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
