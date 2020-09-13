04ef6dc6e3c86abbf92bba1866b8fd505115e627
Mohammed Nawaz <moha4465@mcw0c8p3yy.home>
2020-09-13 15:20:19 +0100

    Add RequestParser and required fields that need to passed through for PUT requests

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "jeff": {"age": 45, "gender": "male"},
-    "alex": {"age": 26, "gender": "male"},
-    "lucy": {"age": 27, "gender": "female"}
-}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+videos = {}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
