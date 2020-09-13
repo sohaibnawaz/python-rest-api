4a46068c31750ae96e787ff00edede9d2509688b
Mohammed Nawaz <moha4465@mcw0c8p3yy.home>
2020-09-13 15:44:50 +0100

    Update Tests to inclue required fileds for PUT, add input as a pause then GET test

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+response = requests.put(BASE + "video/1", {"name": "Grape!", "views": 1000000, "likes": 10})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
